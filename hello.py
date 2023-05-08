ed5515ebd04aa9fb46c82025c31a19eb8d3f5fdd
srics-dev <mshrikanth315@gmail.com>
2023-05-08 12:04:42 +0530

    init

--- hello.py
+++ hello.py
@@ -1,17 +1,25 @@
-import re
-inputs = []
-n = int(input())
-for i in range(n):
-    input_string = input()
-    inputs.append(input_string)
-for input_string in inputs:
-    charecter=re.findall('.',input_string)
-    lenn=len(charecter)
-    if 11==lenn:
-        if re.search('^7',input_string)=='match' or re.search('^8',input_string)=='match' or re.search('^9',input_string):
-            print("Yes")
-        else:
-            print("No")
-    else:
-            print("No") 
-print(lenn)
+# Prompt the user to enter the number of scores
+n = int(input("Enter the number of scores: "))
+
+# Prompt the user to enter the scores
+input_str = input("Enter space-separated scores: ")
+
+# Convert the input string to a list of integers using map
+scores = list(map(int, input_str.split()))
+
+# Sort the list of scores in descending order
+scores.sort(reverse=True)
+
+# Find the runner-up score
+runner_up_score = None
+for score in scores:
+    print(score,scores)
+    if score < scores[0]:
+        runner_up_score = score
+        break
+
+# Print the runner-up score
+if runner_up_score is not None:
+    print("The runner-up score is:", runner_up_score)
+else:
+    print("There is no runner-up score.")
---
+++ student _largest _score_list.py
@@ -0,0 +1,6 @@
+if __name__=='__main__':
+    student=[]
+    for _ in range(int(input())):
+        name=input()
+        score=float(input())
+        
